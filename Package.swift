afa03e8ce348d8d69df96905d2ae48db4b070c47
Sayed Obaid <sayed@valify.me>
2026-03-12 13:14:17 +0200

    Release v2.8.4 - Update SPMPackage

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VIDVOCR",
-            url: "https://valify-public-sdks.s3.eu-central-1.amazonaws.com/VIDVOCR/2.8.4/VIDVOCR.xcframework.zip",
+            url: "https://github.com/Valify-Solutions/VIDVOCR-SPM/releases/download/2.8.4/VIDVOCR.xcframework.zip",
             checksum: "91c5d7ff0698ad5ce4993f9b68ed9b5893dada9b435f0051a04c2e96f684438f"
         )
     ]
